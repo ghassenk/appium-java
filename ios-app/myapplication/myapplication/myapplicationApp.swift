ab0b7cb96eba5858144eea3bc1a2d2138543e529
ghassenk <ghassen.kefi@gmail.com>
2021-05-06 09:23:42 +0200

    adding ios project

---
+++ ios-app/myapplication/myapplication/myapplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  myapplicationApp.swift
+//  myapplication
+//
+//  Created by ghassen kefi on 06/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct myapplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
